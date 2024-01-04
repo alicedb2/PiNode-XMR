1dc59677a42eeb3cfd29ea915d249780975a82f0
Alice Doucet Beaupré <alice.doucet.beaupre@gmail.com>
2024-01-04 14:24:07 -0500

    removed nopasswd tag

--- home/pinodexmr/cpuTemp.sh
+++ home/pinodexmr/cpuTemp.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #CPU temp Status
 
-{ sudo  cat /sys/devices/virtual/thermal/thermal_zone0/temp | awk '{ print ($1 / 1000) "°C" }' & echo "Updates every 60 seconds" & date; } > /var/www/html/temp.txt
+{ cat /sys/devices/virtual/thermal/thermal_zone0/temp | awk '{ print ($1 / 1000) "°C" }' & echo "Updates every 60 seconds" & date; } > /var/www/html/temp.txt
--- home/pinodexmr/system-monitor.sh
+++ home/pinodexmr/system-monitor.sh
@@ -2,19 +2,19 @@
 
 (
 	echo -n "This report generated " & date;
-	echo -n "Status Scripts: " && sudo systemctl status moneroStatus.service | sed -n '3'p | cut -c13-;
-	echo -n "Log-io Server: " && sudo systemctl status log-io-server.service | sed -n '3'p | cut -c13-;
-	echo -n "Log-io File: " && sudo systemctl status log-io-file.service | sed -n '3'p | cut -c13-;	
-	echo -n "Private Node: " && sudo systemctl status moneroPrivate.service | sed -n '3'p | cut -c13-;
-	echo -n "Public  Free: " && sudo systemctl status moneroPublicFree.service | sed -n '3'p | cut -c13-;
-	echo -n "Public RPC Pay: " && sudo systemctl status moneroPublicRPCPay.service | sed -n '3'p | cut -c13-;		
-	echo -n "Mining  Node: " && sudo systemctl status moneroMiningNode.service | sed -n '3'p | cut -c13-;
-	echo -n "Tor     Node: " && sudo systemctl status moneroTorPrivate.service | sed -n '3'p | cut -c13-;
-	echo -n "Tor Public  : " && sudo systemctl status moneroTorPublic.service | sed -n '3'p | cut -c13-;
-	echo -n "I2P     Node: " && sudo systemctl status moneroI2PPrivate.service | sed -n '3'p | cut -c13-;
-	echo -n "Custom  Node: " && sudo systemctl status moneroCustomNode.service | sed -n '3'p | cut -c13-;		
-	echo -n "Explorer    : " && sudo systemctl status blockExplorer.service | sed -n '3'p | cut -c13-;
-	echo -n "P2Pool Server: " && sudo systemctl status p2pool.service | sed -n '3'p | cut -c13-;
-	echo -n "Monero-LWS  : " && sudo systemctl status monero-lws.service | sed -n '3'p | cut -c13-;
-	echo -n "Atomic Swap  : " && sudo systemctl status atomic-swap.service | sed -n '3'p | cut -c13-;
+	echo -n "Status Scripts: " && systemctl status moneroStatus.service | sed -n '3'p | cut -c13-;
+	echo -n "Log-io Server: " && systemctl status log-io-server.service | sed -n '3'p | cut -c13-;
+	echo -n "Log-io File: " && systemctl status log-io-file.service | sed -n '3'p | cut -c13-;
+	echo -n "Private Node: " && systemctl status moneroPrivate.service | sed -n '3'p | cut -c13-;
+	echo -n "Public  Free: " && systemctl status moneroPublicFree.service | sed -n '3'p | cut -c13-;
+	echo -n "Public RPC Pay: " && systemctl status moneroPublicRPCPay.service | sed -n '3'p | cut -c13-;
+	echo -n "Mining  Node: " && systemctl status moneroMiningNode.service | sed -n '3'p | cut -c13-;
+	echo -n "Tor     Node: " && systemctl status moneroTorPrivate.service | sed -n '3'p | cut -c13-;
+	echo -n "Tor Public  : " && systemctl status moneroTorPublic.service | sed -n '3'p | cut -c13-;
+	echo -n "I2P     Node: " && systemctl status moneroI2PPrivate.service | sed -n '3'p | cut -c13-;
+	echo -n "Custom  Node: " && systemctl status moneroCustomNode.service | sed -n '3'p | cut -c13-;
+	echo -n "Explorer    : " && systemctl status blockExplorer.service | sed -n '3'p | cut -c13-;
+	echo -n "P2Pool Server: " && systemctl status p2pool.service | sed -n '3'p | cut -c13-;
+	echo -n "Monero-LWS  : " && systemctl status monero-lws.service | sed -n '3'p | cut -c13-;
+	echo -n "Atomic Swap  : " && systemctl status atomic-swap.service | sed -n '3'p | cut -c13-;
 	) > /var/www/html/iamrunning_version.txt
